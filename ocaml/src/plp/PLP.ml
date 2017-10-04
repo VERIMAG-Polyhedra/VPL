7c13b3bdb370f3380553c30d351f6817fb4c8d79
Alexandre Maréchal <alex.marechal@univ-grenoble-alpes.fr>
2017-10-04 17:27:46 +0200

    Adding a debug

--- ocaml/src/plp/PLP.ml
+++ ocaml/src/plp/PLP.ml
@@ -4,21 +4,21 @@ module Cons = PLPCore.Cons
 module Cert = Cons.Cert
 
 module Debug = DebugTypes.Debug(struct let name = "PLP" end)
-	
+
 module type Type = sig
 	module Minimization : Min.Type
 	module MapV : Map.S with type key = int
 	module PSplx : PSplx.Type
 	module Naming : Naming.Type
-	
+
 	(** This map is used for the certificate generation.
 	Is associates to each simplex column an element of {!type:Cons.t}. *)
 	type 'c mapVar_t = ('c Cons.t) MapV.t
-	
+
 	module Boundary : sig
 		type t
 	end
-	
+
 	module Region : sig
 		type t = {
 			id : int;
@@ -27,31 +27,31 @@ module type Type = sig
 			sx : PSplx.t option(* Tableau de simplexe dont l'objectif a donné cette région *)
 		}
 	end
-	
+
 	type region_t = Cone | NCone
-	
+
 	module ExplorationPoint : sig
-		type t = 
+		type t =
 			| Direction of int * Boundary.t (** (id of origin region, boundary*)
 			| Point of Minimization.Vec.t
 	end
-	
-	type t 
-	
+
+	type t
+
 	type get_next_point_t = region_t -> t -> ExplorationPoint.t option * t
-	
+
 	module RayTracing : sig
-		val get_next_point : get_next_point_t 
+		val get_next_point : get_next_point_t
 	end
-	
+
 	module RegionTesting : sig
-		val get_next_point : get_next_point_t 
+		val get_next_point : get_next_point_t
 	end
-	
+
 	val get_cert_default : 'c Cert.t -> 'c mapVar_t -> PSplx.t -> 'c
-	
+
 	val get_no_cert : 'c Cert.t -> PSplx.t -> 'c
-	
+
 	type config = {
 		get_next_point : get_next_point_t;
 		reg_t : region_t;
@@ -59,22 +59,22 @@ module type Type = sig
 		stgy : PSplx.Objective.pivotStrgyT;
 		regions : Region.t list;
 		}
-	
+
 	val std_config : config
-	
+
 	val run : config -> PSplx.t -> (PSplx.t -> 'c) -> (Region.t * 'c Cons.t) list option
-	
+
 	(** Same as {!val:run} where regions are cones. *)
 	val run_classic : PSplx.t -> (PSplx.t -> 'c) -> (Region.t * 'c Cons.t) list option
 end
 
 module PLP(Minimization : Min.Type) = struct
-	
+
 	module Minimization = Minimization
 	include PLPDistrib.PLP(Minimization)
 
 	let get_cert_default : 'c Cert.t -> 'c mapVar_t -> PSplx.t -> 'c
-		= fun factory map sx ->		
+		= fun factory map sx ->
 		let basisValue = PSplx.getCurVal sx in
 		List.fold_left
 			(fun l (col,q) ->
@@ -87,15 +87,15 @@ module PLP(Minimization : Min.Type) = struct
 			[]
 			basisValue
 		|> Cert.linear_combination factory
-	
+
 	let get_no_cert : 'c Cert.t -> PSplx.t -> 'c
 		= fun factory _ ->
 		factory.Cert.top
-		 
+
 	(*
 	(** We test if exploration points belong to already discovered regions. *)
 	module RegionTesting = struct
-	
+
 		let rec get_next_point_rec : region_t -> ExplorationPoint.t list -> mapRegs_t -> ExplorationPoint.t list
 			= fun reg_t todo regMap ->
 			match todo with
@@ -106,8 +106,8 @@ module PLP(Minimization : Min.Type) = struct
 				else (ExplorationPoint.Point v) :: tl
 				end
 			| _ -> Pervasives.failwith "PLP.RegionTesting.get_next_point_rec"
-		
-		let get_next_point : get_next_point_t 
+
+		let get_next_point : get_next_point_t
 			= fun reg_t plp ->
 			let plp = {plp with todo = get_next_point_rec reg_t plp.todo plp.regs} in
 			if plp.todo = []
@@ -125,24 +125,24 @@ module PLP(Minimization : Min.Type) = struct
 			match point with
 			| ExplorationPoint.Point vec ->
 				let todo = new_points @ (Add_Region.remove_point point plp.todo) in
-				{regs = regs ; todo = todo} 
+				{regs = regs ; todo = todo}
 			| ExplorationPoint.Direction (id, (cstr, pointToExplore)) as point ->
 				let todo = match origin_reg with
-					| Some origin_reg -> 
+					| Some origin_reg ->
 						if Add_Region.should_explore_again origin_reg cstr reg
 						then new_points @ plp.todo
 						else new_points @ (Add_Region.remove_point point plp.todo)
 					| None -> new_points @ plp.todo
 				in
 				{regs = regs ; todo = todo}
-		
+
 	let standard_test : Region.t option -> Region.t -> ExplorationPoint.t -> t -> t
 			= fun _ reg point plp ->
 			Debug.log DebugTypes.Normal (lazy "Adding new region to plp");
 			let regs = MapV.add reg.Region.id reg plp.regs in
 			let todo = Add_Region.remove_point point plp.todo in
-			{regs = regs ; todo = todo} 
-			
+			{regs = regs ; todo = todo}
+
 	let std_config = {
 		add_region = standard;
 		reg_t = Cone;
@@ -150,10 +150,10 @@ module PLP(Minimization : Min.Type) = struct
 		stgy = Objective.Bland;
 		regions = [];
 	}
-	
+
 	let run: config -> PSplx.t -> (PSplx.t -> 'c) -> (Region.t * 'c Cons.t) list option
 		= fun config sx get_cert ->
-		let res = 
+		let res =
 			if PSplx.nParams sx = 0 || !Flags.distributed_plp = None || !Flags.distributed_plp = Some 0
 			then run config sx get_cert
 			else Pervasives.failwith "Distributed PLP not implemented" (*Distributed.run config.stgy sx get_cert*)
@@ -162,11 +162,11 @@ module PLP(Minimization : Min.Type) = struct
 			then match res with
 				| None -> ()
 				| Some regs -> Plot.plot regs
-		end; 
+		end;
 		res
-		
+
 	let run_classic : PSplx.t -> (PSplx.t -> 'c) -> (Region.t * 'c Cons.t) list option
 		= fun sx get_cert ->
 		run std_config sx get_cert
-		
+
 end
