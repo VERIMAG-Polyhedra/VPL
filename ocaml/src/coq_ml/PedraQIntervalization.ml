7844bcbb38a6488996ccb24137423591d2e43c59
Alexandre Maréchal <marechalalex@gmail.com>
2018-06-25 10:55:31 +0200

    Fix: Handelman is correctly called after intervalization

--- ocaml/src/coq_ml/PedraQIntervalization.ml
+++ ocaml/src/coq_ml/PedraQIntervalization.ml
@@ -548,18 +548,6 @@ module Lift (LHD: QInterface.LowLevelDomain) = struct
         | Flags.Intervalization | Flags.Both -> begin
             let ti = c.QAtomicCond.right in
             let te,aft = CQTerm.affineDecompose ti in
-            (*
-            let a' = match te with
-                | CQTerm.Cte c0 ->
-                  (if QNum.isZero c0
-                   then affAssume c.QAtomicCond.cmpOp aft a
-                   else assumeOp false c.QAtomicCond.cmpOp te aft ti a)
-                | _ -> assumeOp false c.QAtomicCond.cmpOp te aft ti a
-            in
-            if !Flags.lin = Flags.Both
-            then LHD.assume [export_cmpT c.QAtomicCond.cmpOp, export_QTerm c.QAtomicCond.right] a'
-            else a'
-            *)
             match te with
                 | CQTerm.Cte c0 -> begin
                     if QNum.isZero c0
@@ -570,7 +558,12 @@ module Lift (LHD: QInterface.LowLevelDomain) = struct
                         then LHD.assume [export_cmpT c.QAtomicCond.cmpOp, export_QTerm c.QAtomicCond.right] a'
                         else a'
                     end
-                | _ -> assumeOp false c.QAtomicCond.cmpOp te aft ti a
+                | _ -> begin
+                    let a' = assumeOp false c.QAtomicCond.cmpOp te aft ti a in
+                    if !Flags.lin = Flags.Both
+                    then LHD.assume [export_cmpT c.QAtomicCond.cmpOp, export_QTerm c.QAtomicCond.right] a'
+                    else a'
+                end
         end
         | Flags.Handelman -> LHD.assume [export_cmpT c.QAtomicCond.cmpOp, export_QTerm c.QAtomicCond.right] a
 (*
